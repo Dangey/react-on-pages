f4c7fc4d4b5ee86554e4b9ae4951ce76cc95dc03
Anthony <32312177+Dangey@users.noreply.github.com>
2022-07-02 12:37:35 -0400

    add Contact route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 const Home = () => <div><h2>Home</h2></div>
 const About = () => <div><h2>About</h2></div>
+const Contact = () => <div><h2>Contact</h2></div>
 
 class App extends Component {
   render() {
@@ -14,10 +15,12 @@ class App extends Component {
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
           </ul>
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </div>
       </Router>
